061e4b48dae49d3271760f12daa2ef1f22be7c7d
Victoria Ward <vward@hawaii.edu>
2023-01-13 19:47:49 -0500

    remove measurement landing page component and references to it

--- projects/nta/src/app/app-routing.module.ts
+++ projects/nta/src/app/app-routing.module.ts
@@ -1,38 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MeasurementLandingPageComponent } from 'tools';
 import { LandingPageComponent } from 'tools';
 import { SingleSeriesComponent } from 'tools';
 import { AnalyzerComponent } from 'tools';
 import { EmbedGraphComponent } from 'tools';
-
-/* const routes: Routes = [
-  // map / to the landing page
-  {
-    path: '',
-    component: MeasurementLandingPageComponent,
-  },
-  {
-    path: 'category',
-    component: MeasurementLandingPageComponent,
-  },
-  {
-    path: 'search',
-    component: MeasurementLandingPageComponent,
-  },
-  {
-    path: 'series',
-    component: SingleSeriesComponent,
-  },
-  {
-    path: 'analyzer',
-    component: AnalyzerComponent,
-  },
-  {
-    path: 'graph',
-    component: EmbedGraphComponent
-  }
-]; */
 
 const routes: Routes = [
   // map / to the landing page
