cd3da20b6899b8e893f981c07decd56abcb1d69e
Leighton Miguel <leim56@hawaii.rr.com>
2024-12-09 15:17:54 -1000

    Staging and Production Deployment Workflows

--- projects/nta/src/environments/environment.staging.ts
+++ projects/nta/src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://stage-api.uhero.hawaii.edu/v1',
+  apiUrl: 'https://api.uhero.hawaii.edu/v1',
   portalUrl: 'https://stage-data.uhero.hawaii.edu/nta/#'
 };
